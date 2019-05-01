ed38970cb4420a9ace5581f958791eebc5bd5169
Mingapril <mk4125@tc.columbia.edu>
2019-05-01 19:03:04 -0400

    changes 0501.2

---
+++ app/src/main/java/com/example/finalproject_mingkyuqingw/fragmentLikes.java
@@ -0,0 +1,21 @@
+package com.example.finalproject_mingkyuqingw;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragmentLikes extends Fragment {
+    public final static String TAG = "Likes Fragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_my_likes,container,false);
+        return view;
+    }
+}
